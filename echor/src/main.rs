a02547221ef504ea760c9bf237de46b8e79ee294
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-08-08 19:31:03 -0400

    test

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,28 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("John Doe <john.doe@example.com")
         .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .min_values(1),
+            )
+        .arg(
+            Arg::with_name("omit_newline")
+            .short('n')
+            .help("Do not print newline")
+            .takes_value(false),
+            )
         .get_matches();
+    let text: Vec<_> = matches.get_many::<String>("text")
+        .expect("`text` is required")
+        .map(|s| s.as_str())
+        .collect();
+    let omit_newline = matches.is_present("omit_newline");
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
